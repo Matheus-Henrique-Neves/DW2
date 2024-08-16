0393501566a35c04a1506091d2a7c9eb14304014
Matheus Henrique Almeida Vieira Neves <matheus16092004@gmail.com>
2024-08-16 10:42:25 -0300

    tentei fazer ficar bonitinho

--- Aula1/Trabalho1.js
+++ Aula1/Trabalho1.js
@@ -3,7 +3,7 @@ var prompt=PromptSync()
 let ValorDatabuada=prompt("qual o numero que vc deseja ver a tabuada? ")
 let vaimutiplica=prompt("até que tabuada vc que que ele chegue ")
 for (let index = 0; index <= vaimutiplica; index++) {
- console.log(`${ValorDatabuada} x ${index} = `+ValorDatabuada*index)
-    
+ console.log(`|${ValorDatabuada}| x |${index}| = `+"|"+ValorDatabuada*index+"|")
+    //faze depois um jeito de ficar bonitinho a poha da taboada
     
 }
